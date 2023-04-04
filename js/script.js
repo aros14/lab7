399c1f6a94667c76c316be2f15967fc0e90f942a
Ana Rosiles <arosiles@hawk.iit.edu>
2023-04-04 12:03:31 -0500

    trying to use functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 function init(){
   function setAlert(){
+    var input = document.getElementById('entryinput');  
     alert('Ana Rosiles' + input);
   }
   var alertBt = document.getElementById('entrybutton');
   alertBt.addEventListener('click', setAlert);
-
-
-
-
-
-
+  alertBt.addEventListener('click', function(){
+    var resultTxt = document.getElementById('textoutput');
+    resultTxt.innerHTML = input;
+  }
 
 window.addEventListener('load', init);

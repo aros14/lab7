4084938e39542b86cdd18cee3ba91f6eb478d560
Ana Rosiles <arosiles@hawk.iit.edu>
2023-04-04 12:29:33 -0500

    fixing formatting

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ function init(){
   function setAlert(){
     var inText = document.getElementById('entryinput');  
     document.getElementById('textoutput').innerHTML = inText.value;
-    alert("Ana Rosiles" + inText.value);
+    alert("Ana Rosiles: " + inText.value);
   }
 
   button.addEventListener('click', setAlert);
+  }
 
 window.addEventListener('load', init);
